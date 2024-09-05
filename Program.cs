9bd5c733676e053af39a1332c728d9e11d44e2ff
TM <info@timelessmedia.nl>
2024-09-05 18:41:18 +0200

    #18: appsettings even aangepast zodat ingesteld staat op productie

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using Duende.IdentityServer.EntityFramework.Interfaces;
 using Microsoft.Extensions.Caching.Memory;
 using QuizTower.IDP;
+using QuizTower.IDP.Util;
 using Serilog;
+using Azure.Identity;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -14,11 +16,20 @@ try
     var builder = WebApplication.CreateBuilder(args);
 
     // Note: Using AddJsonFile with the 'optional: true' parameter means that if the file does not exist, the application will continue running without it.
-    builder.Configuration
-        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-        .AddEnvironmentVariables();
-
+    if (builder.Environment.IsDevelopment() || builder.Environment.IsTest())
+    {
+        builder.Configuration
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+    }
+    else
+    {
+        builder.Configuration
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+    }
+    
     builder.Host.UseSerilog((ctx, lc) => lc
         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
         .Enrich.FromLogContext()
